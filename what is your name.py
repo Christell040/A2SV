2ae138e1f8f33b0a29fb880c7c1f6a86af5aa9e5
Christell040 <100894672+Christell040@users.noreply.github.com>
2023-05-25 19:12:56 +0000

    Add files via upload

---
+++ what is your name.py
@@ -0,0 +1,11 @@
+def print_full_name(first, last):
+    string = f"Hello {first} {last}!You justt delved into python"
+    return string
+
+
+firstname = input("Enter your first name")
+lastname = input("Enter your last name")
+
+# Example usage
+
+print(print_full_name(firstname,lastname))
