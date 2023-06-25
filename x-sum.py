b338b399be12e56f6f81c58f9ac5be717e67c93b
Christell040 <100894672+Christell040@users.noreply.github.com>
2023-06-25 10:12:46 +0000

    Add files via upload

--- x-sum.py
+++ x-sum.py
@@ -1,18 +1,50 @@
-class Solution(object):
-    def maxSum(self, grid):       
-        rows = len(grid)
-        cols = len(grid[0])
-        max_sum = float('-inf')
+def calc_max(board_row, board_col, board):
+    max_sum = 0
+    for r in range(0, board_row):
+        for c in range(0, board_col):
+            current_sum = board[r][c]
+            current_row, current_col = r, c
+            
+            # Upper left
+            while current_row > 0 and current_col > 0:
+                current_row -= 1
+                current_col -= 1
+                current_sum += board[current_row][current_col]
+            
+            current_row, current_col = r, c
+            # Upper right
+            while current_row > 0 and current_col < board_col - 1:
+                current_row -= 1
+                current_col += 1
+                current_sum += board[current_row][current_col]
+            
+            current_row, current_col = r, c
+            # Lower left
+            while current_row < board_row - 1 and current_col > 0:
+                current_row += 1
+                current_col -= 1
+                current_sum += board[current_row][current_col]
+            
+            current_row, current_col = r, c
+            # Lower right
+            while current_row < board_row - 1 and current_col < board_col - 1:
+                current_row += 1
+                current_col += 1
+                current_sum += board[current_row][current_col]
+            
+            max_sum = max(max_sum, current_sum)
+    
+    return max_sum
 
-        for i in range(rows - 2):  # Rows loop
-            for j in range(cols - 2):  # Columns loop
-                # Calculate the sum of the current hourglass
-                current_sum = (
-                    grid[i][j] + grid[i][j + 1] + grid[i][j + 2] +
-                    grid[i + 1][j + 1] +
-                    grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2]
-                )
-                # Update the maximum sum if necessary
-                max_sum = max(max_sum, current_sum)
 
-        return max_sum
+N = int(input())
+
+for _ in range(0, N):
+    dimensions = list(map(int, input().split()))
+    board_row = dimensions[0]
+    board_col = dimensions[1]
+    board = [list(map(int, input().split())) for _ in range(0, board_row)]
+    
+    max_sum = calc_max(board_row, board_col, board)
+            
+    print(max_sum)
