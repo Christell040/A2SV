855570499e6af9fee7d97da2a88dcb7f89b3dd28
Christell040 <100894672+Christell040@users.noreply.github.com>
2023-08-30 10:04:35 +0000

    Add files via upload

---
+++ rangesum1d.py
@@ -0,0 +1,26 @@
+class NumArray(object):
+
+    def __init__(self, nums):
+        self.p_sum=[0]*len(nums)
+
+        for i in range(len(nums)):                     
+            if i == 0:
+                self.p_sum[i] = nums[i]
+            else:
+                self.p_sum[i] = nums[i]+self.p_sum[i-1]
+        
+        
+
+    def sumRange(self, left, right):
+        if left == 0:
+            return self.p_sum[right]
+
+        return self.p_sum[right]-self.p_sum[left-1]
+
+       
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
